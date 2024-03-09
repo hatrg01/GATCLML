45cd28e8fa76bb926e4800043e8d99740c6a58a9
root <hoang.ph303@gmail.com>
2024-03-09 04:40:26 +0000

    update utils

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,22 +1,35 @@
 import os
 import cv2
+from tqdm.auto import tqdm
 
-def get_img(datadir):
-    data = []
-    names = []
-    try:
-        img_names = os.listdir(datadir)
-        for i in img_names:
-            imgdir = datadir + i
-            try:
-                img = cv2.imread(imgdir)
+class Cifar100:
+    def __init__(self, data_dir='../DATA/CIFARv2', phase='train'):
+        self.data_dir = data_dir
+        self.phase = phase
+
+    def load_images(self):
+        images = []
+        image_names = []
+        print(f"Loading Cifar-100 dataset from dir : {self.data_dir} !")
+        data_folder_path = os.path.join(self.data_dir, self.phase)
+        for sub_folder in tqdm(os.listdir(data_folder_path)):
+            sub_folder_path = os.path.join(data_folder_path, sub_folder)
+            for img_name in os.listdir(sub_folder_path):
+                img_path = os.path.join(sub_folder_path, img_name)
+                img = cv2.imread(img_path)
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                # img = cv2.resize(img, (200,200))
-                data.append(img)
-                names.append(i)
-            except AttributeError:
-                print('No image')
-    except AttributeError:
-        print('No folder')
+                images.append(img)
 
-    return data, names
+                img_name = f"{self.phase}/{sub_folder}/{img_name}"
+                image_names.append(img_name)
+        return images, image_names
+
+loader = Cifar100()
+images, image_names = loader.load_images()
+
+print(len(images))
+print(len(image_names))
+print(type(images[0]))
+print(images[0].shape)
+print(image_names[100])
+
