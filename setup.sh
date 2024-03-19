8f45c24c330f14325e2521ffc6bf6b6690ca58de
root <hoang.ph303@gmail.ccom>
2024-03-19 15:13:23 +0000

    update setup file

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ pip install torch_geometric
 pip install pyg-lib -f https://data.pyg.org/whl/nightly/torch-2.1.0+cu121.html
 pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.1.0+cu121.html
 
-unzip /content/drive/MyDrive/RESEARCH/DATA/CIFARv2.zip -d ./DATA
+MKDIR ./DATA
+unzip /content/drive/MyDrive/RESEARCH/DATA/CIFARv2Mini.zip -d ./DATA
